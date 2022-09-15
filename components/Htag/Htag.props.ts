d1fc94f14f1fdb42695fe48ac7a5b9102233f128
Belousov <nibelousov@neoflex.ru>
2022-09-15 16:35:57 +0300

    fe: [feature/OWL-001] - создан компонент заголовка, файл _document.tsx

---
+++ components/Htag/Htag.props.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export interface HtagProps {
+    tag: string;
+    children: ReactNode;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import Head from "next/head";
 import { AppProps } from "next/dist/shared/lib/router/router";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>OwlTop - наш лучший топ!</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+
 }
 
 export default MyApp;
