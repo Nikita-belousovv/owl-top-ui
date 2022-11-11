84be4461f0c39a9890ea64145761ccb95c232914
Belousov <nibelousov@neoflex.ru>
2022-11-11 11:19:28 +0300

    feature: [OWL-005] - позиционирование UI компонентов | адаптив | date-fns

--- Layout/Sidebar/Sidebar.tsx
+++ Layout/Sidebar/Sidebar.tsx
@@ -5,8 +5,8 @@ import styles from './Layout.module.css';
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
     return (
-		<div {...props}>
+		<aside {...props}>
 			Sidebar
-		</div>
+		</aside>
 	);
 };
--- components/Ptag/Ptag.props.ts
+++ components/Ptag/Ptag.props.ts
@@ -3,4 +3,5 @@ import React, {ReactNode } from "react";
 export interface PtagProps extends React.ComponentProps<'p'> {
     children: ReactNode;
     type: string;
+    color?: string;
 }
