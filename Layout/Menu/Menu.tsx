2c0469d648d536913c08158f1c5c6dc5cfdf7696
Belousov <nibelousov@neoflex.ru>
2023-02-04 16:45:46 +0300

    feature: [OWL-008] - реализовано открытие/закрытие элементов 2 - го уровня меню

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -1,7 +1,10 @@
 import { useContext } from 'react';
 
+import Link from 'next/link';
+import { useRouter } from "next/router";
+
 import { MainComponentCxt } from '../../context/app.xontext';
-import { FirstLevelMenuItem, PageItem } from "../../interfaces/menu.interface";
+import { FirstLevelMenuItem, MenuItem, PageItem } from "../../interfaces/menu.interface";
 import { TopLevelCategory } from "../../interfaces/page.interface";
 
 import LessonIcon from './Icons/lesson.svg';
@@ -46,19 +49,42 @@ export const Menu = (): JSX.Element => {
 		setMenu,
 		firstCategory
 	} = useContext(MainComponentCxt);
+	const router = useRouter();
+
+	const equalToCurrentRoute = (menuSecItem: MenuItem) => {
+		if(menuSecItem.pages.map(page => page.alias).includes(router.asPath.split('/')[2])) {
+			return menuSecItem.isOpened = true;
+		}
+	};
+
+	const openSecondLevel = (currentSecCategory: string) => {
+		const isRightCurrentCategory = menu.map((menuItem): MenuItem => {
+			if(menuItem._id.secondCategory === currentSecCategory) {
+				menuItem.isOpened = !menuItem.isOpened;
+			} else {
+				menuItem.isOpened = false;
+			}
+
+			return menuItem;
+		});
+
+		setMenu && setMenu(isRightCurrentCategory);
+	};
 
 	const buildLevelMenu = (): JSX.Element => (
 		<>
 			{firstLevelMenu.map(firstLvlItem => 
 				<div key={firstLvlItem.route}>
-					<a href={`/${firstLvlItem.route}`}>
-						<div className={cn(styles.firstLevel, {
-							[styles.FLActive]: firstLvlItem.id === firstCategory
-						})}>
-							{firstLvlItem.icon}
-							<span>{firstLvlItem.name}</span>
-						</div>
-					</a>
+					<Link href={`/${firstLvlItem.route}`}>
+						<a>
+							<div className={cn(styles.firstLevel, {
+								[styles.FLActive]: firstLvlItem.id === firstCategory
+							})}>
+								{firstLvlItem.icon}
+								<span>{firstLvlItem.name}</span>
+							</div>
+						</a>
+					</Link>
 					<>{firstLvlItem.id === firstCategory && buildSecondLevel(firstLvlItem)}</>
 				</div>
 			)}
@@ -67,25 +93,37 @@ export const Menu = (): JSX.Element => {
 
 	const buildSecondLevel = (menuSecond: FirstLevelMenuItem): JSX.Element => (
 		<div className={styles.secLevelBlock}>
-			{menu.map(mnuSecItem => <div key={mnuSecItem._id.secondCategory}>
-				<div className={cn({
-					[styles.secLevBlockOpened]: mnuSecItem.isOpened
-				})}>
-					<div className={styles.secLevel}>
-						{mnuSecItem._id.secondCategory}
+			{menu.map(mnuSecItem => {
+				equalToCurrentRoute(mnuSecItem);
+
+				return (
+					<div key={mnuSecItem._id.secondCategory}>
+						<div
+							className={styles.secLevel}
+							onClick={() => openSecondLevel(mnuSecItem._id.secondCategory)}
+						>
+							{mnuSecItem._id.secondCategory}
+						</div>
+						<div className={cn(styles.secLevBlock, {
+							[styles.secLevBlockOpened]: mnuSecItem.isOpened
+						})}>
+							{buildThirdLevel(mnuSecItem.pages, menuSecond.route)}
+						</div>
 					</div>
-					{buildThirdLevel(mnuSecItem.pages, menuSecond.route)}
-				</div>
-			</div>)}
+				);
+			})}
 		</div>
 	);
 
 	const buildThirdLevel = (pages: PageItem[], route: string): JSX.Element[] => 
-		(pages.map(page => <a key={page.alias} href={`/${route}/${page.alias}`} className={cn(styles.thirdLevel, {
-			[styles.activeThLevel]: false
-		})}>
-			{page.category}
-		</a>)
+		(pages.map(page => 
+			<Link key={page.alias} href={`/${route}/${page.alias}`}>
+				<a className={cn(styles.thirdLevel, {
+					[styles.activeThLevel]: `/${route}/${page.alias}` === router.asPath
+				})}>
+					{page.category}
+				</a>
+			</Link>)
 	);
 
     return (
